e31a3cc8d6ac5cb7c4732f1d0b3b9ebaed399a66
Simon Whatley <simonwhatley@gmail.com>
2020-08-04 13:46:36 +0100

    Updates

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,46 @@ router.get('/', (req, res) => {
 })
 
 // ----------------------------------------------------------------------------
+// Landing page
+// ----------------------------------------------------------------------------
+
+router.get('/coronavirus', (req, res) => {
+  res.render('index', {
+    actions: {
+      home: `${req.baseUrl}/`
+    }
+  })
+})
+
+// ----------------------------------------------------------------------------
+// Hubs
+// ----------------------------------------------------------------------------
+
+router.get('/coronavirus/business-support', (req, res) => {
+  res.render('business-support', {
+    actions: {
+      home: `${req.baseUrl}/`
+    }
+  })
+})
+
+router.get('/coronavirus/worker-support', (req, res) => {
+  res.render('worker-support', {
+    actions: {
+      home: `${req.baseUrl}/`
+    }
+  })
+})
+
+router.get('/coronavirus/education-and-childcare', (req, res) => {
+  res.render('education-and-childcare', {
+    actions: {
+      home: `${req.baseUrl}/`
+    }
+  })
+})
+
+// ----------------------------------------------------------------------------
 // Add routes above this line /////////////////////////////////////////////////
 // ----------------------------------------------------------------------------
 
